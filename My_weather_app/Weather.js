52cf305641c93668fc863459fb6c7bc4b1274a92
Porter Peterson <porterpeterson@Porters-MacBook-Air.local>
2023-11-08 21:35:30 -0700

    finished project

--- My_weather_app/Weather.js
+++ My_weather_app/Weather.js
@@ -1,1 +1,21 @@
+function NewTemp(farenheit) {
+  const celsius = farenheit - 32;
+  return celsius;
+}
+
+function createMessage(farenheit, celsius) {
+  return (
+    "You put in " +
+    farenheit +
+    " farenheit, " +
+    "you get " +
+    celsius +
+    " celsius."
+  );
+}
+
 const convertToCelsius = window.prompt("Enter the amount of farenheit");
+
+const convertedCelsius = NewTemp(convertToCelsius);
+
+console.log(createMessage(convertToCelsius, convertedCelsius));
